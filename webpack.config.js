01eea3b6f92249d0bc70b3c6d1572ccf4123641c
shawn-thecool <shawn-dev@naver.com>
2019-12-12 14:56:25 +0900

    Feat : 초기 셋팅 완료

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  // enntry file
+  entry: "./src/main.js",
+  // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  mode: "development"
+};
